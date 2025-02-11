0273d080dc83ffcc6e71bf7a14cddcefda91390d
Vishal-Borse <vishalsborse8@gmail.com>
2025-02-11 10:38:01 +0530

    deployment chnages

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Frontend URL
+    origin: "https://readmoree.com", // Frontend URL
     credentials: true, // Allow cookies to be sent
   })
 );
